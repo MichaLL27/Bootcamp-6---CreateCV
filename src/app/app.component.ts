f1957dae85a38cca5915825391c2bf4af03546e0
MichaLL27 <lazarashvili2704@gmail.com>
2023-02-03 18:09:39 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div class="allComponent">
+      <mainComponent></mainComponent>
+      <router-outlet> </router-outlet>
+    </div>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Bootcamp6';
-}
+export class AppComponent {}
---
+++ src/app/mainPage/main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mainComponent',
+  templateUrl: 'main.component.html',
+  styleUrls: ['./main.component.scss'],
+})
+export class mainComponent {}
