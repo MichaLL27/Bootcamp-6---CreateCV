732fb5ef3ed4d27a3122ac188fd3ecf50d3276f6
MichaLL27 <Lazarashvili2704@gmail.com>
2023-02-05 01:59:56 +0400

    new experience router

--- src/app/PersonalInformation/personal.component.ts
+++ src/app/PersonalInformation/personal.component.ts
@@ -2,5 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: 'personal.component.html',
+  styleUrls: ['./personal.component.scss'],
 })
 export class personalInfoComponent {}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
 import { MainComponent } from './mainPage/main.component';
 import { personalInfoComponent } from './PersonalInformation/personal.component';
 
 export const appRoutes: Routes = [
   { path: 'Main', component: MainComponent },
   { path: 'Personalinformation', component: personalInfoComponent },
+  { path: 'Experience', component: ExperienceComponent },
   { path: '', redirectTo: '/Main', pathMatch: 'full' },
 ];
