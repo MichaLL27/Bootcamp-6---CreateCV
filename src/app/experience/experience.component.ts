6b00f0bfbe40537737f2fc6a3816a7bfedd5069a
MichaLL27 <Lazarashvili2704@gmail.com>
2023-02-05 06:18:39 +0400

    fix min problem

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   templateUrl: 'experience.component.html',
+  styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent {}
